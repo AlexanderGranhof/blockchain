7015ededd1320b7130786298a1599a0be4074dd3
Alexander Granhof <alexander.g737@gmail.com>
2021-02-01 13:05:29 +0100

    created the express api to interact with the blockchain

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,59 @@
-import { SSL_OP_NO_COMPRESSION } from 'constants'
+import express from 'express'
+import Blockchain from './services/blockchain'
 
-console.log('blockchain')
+const app = express()
+const blockchain = new Blockchain()
+
+app.use(express.json())
+app.use(express.text())
+
+app.get('/', (req, res) => res.send('blockchain'))
+
+app.get('/register_node', (req, res) => {
+    const valid = blockchain.registerNode(req.body)
+
+    if (!valid) {
+        return res.send('invalid node')
+    }
+
+    return res.sendStatus(200)
+})
+
+app.get('/chain', (req, res) => {
+    return res.json(blockchain.chain)
+})
+
+app.post('/createTransaction', async (req, res) => {
+    const { sender, recipient, amount } = req.body
+
+    if (!(sender && recipient && amount)) {
+        return res.send(false)
+    }
+
+    const success = await blockchain.createTransaction(sender, recipient, amount)
+
+    res.send(success)
+})
+
+app.get('/mine', (req, res) => {
+    const proof = blockchain.mineBlock()
+
+    res.send(proof.toString())
+})
+
+app.post('/createBlock', async (req, res) => {
+    console.log(req.body)
+    const created = await blockchain.createBlock(req.body)
+
+    console.log(created)
+
+    if (created) {
+        return res.send('created a block')
+    }
+
+    return res.send('did not create a block')
+})
+
+app.listen(3000, () => {
+    console.log('app is running')
+})
